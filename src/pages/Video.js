3c31a2e16a4f182a377a0e6634ec1ca5aea7eac9
Leonardo Firmo <leonardofirmodamota@gmail.com>
2023-06-14 20:30:09 -0300

    Projeto iniciado

---
+++ src/pages/Video.js
@@ -0,0 +1,32 @@
+import React, { useRef,useState } from "react";
+import "./Video.css";
+
+function Video() {
+  const videoRef = useRef(null)
+  const [play, setPlay] = useState(false)
+  
+  function handleStart (){
+    if(!play){
+      setPlay(true)
+      videoRef.current.play()
+      return
+    }
+    
+    videoRef.current.pause()
+    setPlay(false)
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handleStart}
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        loop
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
