5dec73312e931b0cd7a5bd7727a982c90fa56892
Leonardo Firmo <leonardofirmodamota@gmail.com>
2023-06-15 11:16:48 -0300

    Funcionalidade de pausar o video anterior ao dar scroll  adicionada.

--- src/pages/Video/Video.js
+++ src/pages/Video/Video.js
@@ -11,21 +11,27 @@ function Video() {
       videoRef.current.play()
       return
     }
-    
+ 
     videoRef.current.pause()
     setPlay(false)
   }
 
+  function handleNextVideo (){
+    videoRef.current.pause()
+    setPlay(false)
+    
+  }
+ 
   return (
     <div className="video">
-      <video
+      <video 
         className="video__player"
         ref={videoRef}
         onClick={handleStart}
+        onWheel={handleNextVideo}
         src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
         loop
       >
-        
       </video>
     </div>
   );
