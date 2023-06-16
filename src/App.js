e466df2ba80a015f69dfeeaf62e0ec6af97de1fc
Leonardo Firmo <leonardofirmodamota@gmail.com>
2023-06-16 19:42:37 -0300

    Adicionado siderbar e props

--- src/App.js
+++ src/App.js
@@ -8,9 +8,35 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video/>
-        <Video/>
-        <Video/>
+        <Video
+          likes={100}
+          messages={200}
+          shares={300}
+          name="Leo"
+          description="Gato Goleiro"
+          music="música épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        <Video
+          likes={23}
+          messages={444}
+          shares={500}
+          name="Leo"
+          description="Gato olhando para camera"
+          music="música country"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
+        />
+
+        <Video
+          likes={353}
+          messages={250}
+          shares={25}
+          name="Leo"
+          description="Gato Pulando"
+          music="música épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        
       </div>
     </div>
   );
--- src/components/footer/VideoFooter.js
+++ src/components/footer/VideoFooter.js
@@ -1,29 +1,27 @@
 import React from "react";
 import "./VideoFooter.css";
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
 
-function VideoFooter() {
+function VideoFooter({ name, description, music }) {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
-        <h3>@Leonardo Firmo</h3>
-        <p>Gato pulando</p>
-        <div className="videoFooter__music"> 
-          <MusicNoteIcon className="videoFooter__icon"/>
+        <h3>@{name}</h3>
+        <p>{description}</p>
+        <div className="videoFooter__music">
+          <MusicNoteIcon className="videoFooter__icon" />
 
           <div className="videoFooterMusic__text">
-            <p>Título da musica</p>
+            <p>{music}</p>
           </div>
-          
         </div>
       </div>
 
-      <img 
+      <img
         className="videoFooter__record"
-        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png?t=2023-05-22T19%3A39%3A28.772Z" 
-        alt="Imagem de um vinil girando" 
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png?t=2023-05-22T19%3A39%3A28.772Z"
+        alt="Imagem de um vinil girando"
       />
-
     </div>
   );
 }
